c41d19303882a2cffe0a054144ac7ed4a0d7cdf8
Jxck <block.rxckin.beats@gmail.com>
2015-03-06 16:06:52 +0900

    add test for %{file}

---
+++ test/filename.js
@@ -0,0 +1,76 @@
+var Logger = Logger || require('../').Logger;
+
+function assert(act, exp, msg) {
+  console.log('.');
+  console.assert(act === exp, msg);
+}
+
+var trace = console.trace;
+var debug = console.debug;
+var info = console.info;
+var warn = console.warn;
+var error = console.error;
+var fatal = console.fatal;
+
+console.fatal = function(log) {
+  assert(log, "filename.js:45");
+}
+
+console.error = function(log) {
+  assert(log, "filename.js:48");
+}
+
+console.warn = function(log) {
+  assert(log, "filename.js:51");
+}
+
+console.info = function(log) {
+  assert(log, "filename.js:57");
+}
+
+console.debug = function(log) {
+  assert(log, "filename.js:64");
+}
+
+console.trace = function(log) {
+  assert(log, "filename.js:68");
+}
+
+console.log('test_filename');
+var logger = new Logger('test_filename', {
+  format: '%file'
+});
+
+
+logger.fatal();
+
+function outer() {
+  logger.error();
+
+  function inner() {
+    logger.warn();
+  }
+  inner();
+}
+
+function base() {
+  logger.info();
+  outer();
+}
+
+base();
+
+setTimeout(function() {
+  logger.debug();
+}, 10);
+
+for (var i=0; i<1; i++) {
+  logger.trace();
+}
+
+console.trace = trace;
+console.debug = debug;
+console.info  = info;
+console.warn  = warn;
+console.error = error;
+console.fatal = fatal;
